5d227796e7277f625c5a49420e74f19143f3f9e7
flybear <365998570@qq.com>
2015-12-09 16:06:09 +0800

    update

--- demo/components/Dashboard.js
+++ demo/components/Dashboard.js
@@ -2,11 +2,15 @@
  * Created by flybear on 15/11/18.
  */
 import React from 'react';
+import { Link } from 'react-router';
 
 class Dashboard extends React.Component{
     render(){
         return (
-            <div>This is Dashboard!</div>
+            <div>
+                This is Dashboard!
+                <Link to="/about">About</Link>
+            </div>
         )
     }
 }
--- demo/main.js
+++ demo/main.js
@@ -1,52 +1,3 @@
-///**
-// * Created by flybear on 15/11/12.
-// */
-//
-//import React from 'react';
-//import { render } from 'react-dom';
-//import { Router, Route, Link } from 'react-router';
-//import Head from './structure/head.jsx';
-//import Footer from './structure/footer.jsx';
-//
-//const App = React.createClass({
-//    render(){
-//        return (
-//            <div>
-//                <Head />
-//                    {this.props.children}
-//                <Footer />
-//            </div>
-//        )
-//    }
-//});
-//
-//const About = React.createClass({
-//    render(){
-//        return (
-//            <div>This is About!</div>
-//        )
-//    }
-//});
-//
-//const Users = React.createClass({
-//    render(){
-//        return (
-//            <div>This is Users!</div>
-//        )
-//    }
-//});
-//
-//const routes = {
-//    path: '/',
-//    component: App,
-//    childRoutes: [
-//        { path: 'about', component: About },
-//        { path: 'users', component: Users }
-//    ]
-//};
-//
-//render(<Router routes={routes} />, document.getElementById('app'))
-
 
 import React from 'react'
 import { render } from 'react-dom'
@@ -54,9 +5,10 @@ import { createHistory, useBasename } from 'history'
 import { Router } from 'react-router'
 import App from './components/App'
 
-const history = useBasename(createHistory)({
-    basename: '/huge-apps'
+let history = useBasename(createHistory)({
+    basename: '/'
 });
+
 
 const rootRoute = {
     path: '/',
